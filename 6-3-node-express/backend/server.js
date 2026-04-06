3df6213890597c2aade6ef84a3db590953622724
Abdullah AL Otaibi <s202322890@kfupm.edu.sa>
2026-04-06 12:01:21 +0300

    done

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,18 +1,30 @@
 // TODO 1: Import and Create express app instance
+import express from "express";
+import { getRandomQuote } from "./quotes.js";
+const app = express();
 
 // TODO 1: Define server port
-
+const PORT = 3000;
 
 // TODO 4: Import and Apply CORS middleware
-
+import cors from "cors";
+app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
-
+import morgan from "morgan";
+app.use(morgan("dev"));
 
 // TODO 6.1: Create root route "/"
-
+app.get("/", (req, res) => {
+  res.send("Hello from the server!");
+});
 
 // TODO 6.2: Create "/api/quote" route
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
 
 
 // TODO 7: Start server using app.listen
+app.listen(3000, () => console.log("Server running"));
